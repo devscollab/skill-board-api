9151cb623162e8db2874cf2518808866c846bd36
Aditya Dawadikar <adityadawadikar2000@gmail.com>
2020-08-27 09:20:22 +0530

    registration fixed with password hashing

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -1,104 +1,127 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+
 const Student = require("../models/student");
 const SuperUser = require("../models/superuser");
 
 exports.registerStudent = (req, res) => {
-  console.log(req.file);
-  const student = new Student({
-    _id: new mongoose.Types.ObjectId(),
-    Login: {
-      email: req.body.Login.email,
-      password: req.body.Login.password,
-    },
-    Personal: {
-      name: req.body.Personal.name,
-      college: req.body.Personal.college,
-      department: req.body.Personal.department,
-      year: req.body.Personal.year,
-      division: req.body.Personal.division,
-      rollno: req.body.Personal.rollno,
-    },
-    Social: {
-      phone: req.body.Social.phone,
-      linkedin: req.body.Social.linkedin,
-      github: req.body.Social.github,
-      personalwebsite: req.body.Social.personalwebsite,
-      resume: req.body.Social.resume,
-      iswhatsaap: req.body.Social.iswhatsaap,
-    },
-    Skills: {
-      skill: req.body.Skills.skill,
-      projectsforskills: req.body.Skills.projectsforskills,
-      topskill: req.body.Skills.topskill,
-      cgpa: req.body.Skills.cgpa,
-    },
-    Optionals: {
-      introduction: req.body.Optionals.introduction,
-      gender: req.body.Optionals.gender,
-      age: req.body.Optionals.age,
-      mother_tongue: req.body.Optionals.mother_tongue,
-      languages_known: req.body.Optionals.languages_known,
-    },
-    MetaData: {
-      hasAdminAccess: req.body.Optionals.hasAdminAccess,
-      github_metadata_object: req.body.Optionals.github_metadata_object,
-    },
-  });
-  console.log("Student");
-  student.save()
-    .then(doc => {
-      res.status(200).json({doc :doc,
-        message:"Student resgistration successful"
-      });
+    const password = req.body.Login.password;
+    bcrypt.hash(password, 10, (err, result) => {
+        if (err) {
+            res.status(500).json({
+                message: "some error occured while storing credentials",
+                error: err
+            })
+        }
+        if (result) {
+            const student = new Student({
+                _id: new mongoose.Types.ObjectId(),
+                Login: {
+                    email: req.body.Login.email,
+                    password: result,
+                },
+                Personal: {
+                    name: req.body.Personal.name,
+                    college: req.body.Personal.college,
+                    department: req.body.Personal.department,
+                    year: req.body.Personal.year,
+                    division: req.body.Personal.division,
+                    rollno: req.body.Personal.rollno,
+                },
+                Social: {
+                    phone: req.body.Social.phone,
+                    linkedin: req.body.Social.linkedin,
+                    github: req.body.Social.github,
+                    personalwebsite: req.body.Social.personalwebsite,
+                    resume: req.body.Social.resume,
+                    iswhatsaap: req.body.Social.iswhatsaap,
+                },
+                Skills: {
+                    skill: req.body.Skills.skill,
+                    projectsforskills: req.body.Skills.projectsforskills,
+                    topskill: req.body.Skills.topskill,
+                    cgpa: req.body.Skills.cgpa,
+                },
+                Optionals: {
+                    introduction: req.body.Optionals.introduction,
+                    gender: req.body.Optionals.gender,
+                    age: req.body.Optionals.age,
+                    mother_tongue: req.body.Optionals.mother_tongue,
+                    languages_known: req.body.Optionals.languages_known,
+                },
+                MetaData: {
+                    hasAdminAccess: req.body.MetaData.hasAdminAccess,
+                    github_metadata_object: req.body.MetaData.github_metadata_object,
+                },
+            });
+
+            student.save()
+                .then(doc => {
+                    res.status(200).json({
+                        message: "Student resgistration successful",
+                        doc: doc,
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json({
+                        message: "some error occured while storing user",
+                        error: err,
+                    });
+                });
+        }
     })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
 };
 
 exports.registerSuperuser = (req, res) => {
-    console.log(req.body);
-    const superuser = new SuperUser({
-        _id: new mongoose.Types.ObjectId(),
-        Login: {
-          email: req.body.Login.email,
-          password: req.body.Login.password,
-        },
-        Personal: {
-          name: req.body.Personal.name,
-        },
-        Social: {
-          phone: req.body.Social.phone,
-          linkedin: req.body.Social.linkedin,
-          github: req.body.Social.github,
-        },
-        Optionals: {
-          introduction: req.body.Optionals.introduction,
-          gender: req.body.Optionals.gender,
-          age: req.body.Optionals.age,
-          mother_tongue: req.body.Optionals.mother_tongue,
-          languages_known: req.body.Optionals.languages_known,
-        },
-        MetaData: {
-          hasAdminAccess: req.body.MetaData.hasAdminAccess,
-          github_metadata_object: req.body.MetaData.github_metadata_object,
-        },
-      });
-  console.log("SuperUser");
-  superuser.save()
-        .then(doc => {
-          res.status(200).json({doc :doc,
-            message:"superuser resgistration successful"
-          });
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(500).json({
-            error: err,
-          });
-        });
-
+    const password = req.body.Login.password;
+    bcrypt.hash(password, 10, (err, result) => {
+        if (err) {
+            res.status(500).json({
+                message: "some error occued while storing user credentials",
+                error: err
+            })
+        }
+        if (result) {
+            const superuser = new SuperUser({
+                _id: new mongoose.Types.ObjectId(),
+                Login: {
+                    email: req.body.Login.email,
+                    password: result,
+                },
+                Personal: {
+                    name: req.body.Personal.name,
+                },
+                Social: {
+                    phone: req.body.Social.phone,
+                    linkedin: req.body.Social.linkedin,
+                    github: req.body.Social.github,
+                },
+                Optionals: {
+                    introduction: req.body.Optionals.introduction,
+                    gender: req.body.Optionals.gender,
+                    age: req.body.Optionals.age,
+                    mother_tongue: req.body.Optionals.mother_tongue,
+                    languages_known: req.body.Optionals.languages_known,
+                },
+                MetaData: {
+                    hasAdminAccess: req.body.MetaData.hasAdminAccess,
+                    github_metadata_object: req.body.MetaData.github_metadata_object,
+                },
+            });
+            superuser.save()
+                .then(doc => {
+                    res.status(200).json({
+                        message: "superuser resgistration successful",
+                        doc: doc
+                    });
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        message: "some error occured while storing the data to the database",
+                        error: err
+                    });
+                });
+        }
+    })
 };
--- app/models/student.js
+++ app/models/student.js
@@ -4,136 +4,132 @@ var Float2 = require("mongoose-float").loadType(mongoose, 2);
 
 // Not Sure how to use Timestamps in Mongoose ,so please check it
 const schemaOptions = {
-  timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
 };
 
 const studentSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    Login: {
-      email: {
-        type: String,
-        required: true,
-        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
-      },
-      password: {
-        type: String,
-        required: true,
-      },
-    },
-    Personal: {
-      name: {
-        type: String,
-        required: true,
-      },
-      college: {
-        type: String,
-        required: true,
-      },
-      department: {
-        type: String,
-        required: true,
-      },
-      year: {
-        type: String,
-        required: true,
-      },
-      division: {
-        type: String,
-        // required: true,
-        // Was not sure to keep it mandatory
-      },
-      rollno: {
-        type: String,
-        required: true,
-      },
-    },
-    Social: {
-      phone: {
-        type: Number,
-        match: /^[0-9]{10}$/,
-        required: true,
-      },
-      linkedin: {
-        type: String,
-        required: true,
-        match: /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi,
-      },
-      github: {
-        type: String,
-        required: true,
-        match: /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi,
-      },
-      personalwebsite: {
-        type: String,
-        match: /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi,
-      },
-      resume: {
-        type: String,
-        match: /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi,
-      },
-      iswhatsaap: {
-        type: Boolean,
-        required: true,
-      },
-    },
-    // All required
-    Skills: {
-      skills: [
-        {
-          type: String,
-          required: true,
+        _id: mongoose.Schema.Types.ObjectId,
+        Login: {
+            email: {
+                type: String,
+                required: true,
+                match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+            },
+            password: {
+                type: String,
+                required: true,
+            },
         },
-      ],
-      projectsforskills: [
-        {
-          type: String,
-          match: /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi,
-          required: true,
+        Personal: {
+            name: {
+                type: String,
+                required: true,
+            },
+            college: {
+                type: String,
+                required: true,
+            },
+            department: {
+                type: String,
+                required: true,
+            },
+            year: {
+                type: String,
+                required: true,
+            },
+            division: {
+                type: String,
+                // required: true,
+                // Was not sure to keep it mandatory
+            },
+            rollno: {
+                type: String,
+                required: true,
+            },
         },
-      ],
-      topskill: {
-        type: String,
-        required: true,
-      },
-      cgpa: {
-        type: Float2,
-        required: true,
-      },
+        Social: {
+            phone: {
+                type: Number,
+                match: /^[0-9]{10}$/,
+                required: true,
+            },
+            linkedin: {
+                type: String,
+                required: true,
+                match: /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi,
+            },
+            github: {
+                type: String,
+                required: true,
+                match: /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi,
+            },
+            personalwebsite: {
+                type: String,
+                match: /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi,
+            },
+            resume: {
+                type: String,
+                match: /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi,
+            },
+            iswhatsaap: {
+                type: Boolean,
+                required: true,
+            },
+        },
+        // All required
+        Skills: {
+            skills: [{
+                type: String,
+                required: true,
+            }, ],
+            projectsforskills: [{
+                type: String,
+                match: /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi,
+                required: true,
+            }, ],
+            topskill: {
+                type: String,
+                required: true,
+            },
+            cgpa: {
+                type: Float2,
+                required: true,
+            },
+        },
+        Optionals: {
+            introduction: {
+                type: String,
+            },
+            gender: {
+                type: String,
+                enum: ["Male", "Female", "Other"],
+                required: true,
+            },
+            age: {
+                type: Number,
+                min: 10,
+                max: 100,
+                required: true,
+            },
+            mother_tongue: {
+                type: String,
+            },
+            languages_known: [{ type: String }],
+        },
+        MetaData: {
+            hasAdminAccess: {
+                type: Boolean,
+                required: true,
+            },
+            github_metadata_object: {
+                type: Object,
+            },
+        },
     },
-    Optionals: {
-      introduction: {
-        type: String,
-      },
-      gender: {
-        type: String,
-        enum: ["Male", "Female", "Other"],
-        required: true,
-      },
-      age: {
-        type: Number,
-        min: 10,
-        max: 100,
-        required: true,
-      },
-      mother_tongue: {
-        type: String,
-      },
-      languages_known: [{ type: String }],
-    },
-    MetaData: {
-      hasAdminAccess: {
-        type: Boolean,
-        required: true,
-      },
-      github_metadata_object: {
-        type: Object,
-      },
-    },
-  },
-  schemaOptions
+    schemaOptions
 );
 
-module.exports = mongoose.model("Student", studentSchema);
+module.exports = mongoose.model('Student', studentSchema);
 
 // I have made a few changes in types I have mentioned them below
 
