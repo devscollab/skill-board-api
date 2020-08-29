ddcbde6b9eb4c05d3a71926d663c0f0989b3f2aa
Aditya Dawadikar <adityadawadikar2000@gmail.com>
2020-08-29 18:43:53 +0530

    updation logic required

--- app/controllers/student.js
+++ app/controllers/student.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Student = require("../models/student");
+const { compareSync } = require('bcrypt');
 
 exports.getStudentProfileById = (req, res) => {
     Student.find({ _id: req.params.id })
@@ -35,11 +36,13 @@ exports.getAllStudentProfiles = (req, res) => {
 }
 
 exports.updateStudentProfileById = (req, res) => {
+    /*
     const id = req.params.id;
     const updateOperations = req.body;
     for (const operations in updateOperations) {
         updateOperations[operations.propName] = operations.value;
     }
+    
     Student.update({ _id: id }, { $set: updateOperations })
         .exec()
         .then(doc => {
@@ -54,6 +57,12 @@ exports.updateStudentProfileById = (req, res) => {
                 error: err
             })
         });
+    */
+    try {
+        findandupdate(req, res) //, id, updateOperations);
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 exports.deleteStudentProfileById = (req, res) => {
@@ -91,4 +100,33 @@ exports.getStudentsByQuery = (req, res) => {
                 error: err
             })
         })
+}
+
+
+async function findandupdate(req, res) {
+    const id = req.params.id;
+    const updateOperations = req.body;
+    for (const operations in updateOperations) {
+        updateOperations[operations.propName] = operations.value;
+    }
+    await Student.findOne({ _id: id })
+        .then(doc => {
+            updationKeys = Object.keys(updateOperations);
+            console.log(updationKeys)
+                //need some code here to fix overriding
+        })
+    await Student.updateOne({ _id: id }, { $set: updateOperations })
+        .exec()
+        .then(doc => {
+            res.status(200).json({
+                message: "successfully updated",
+                docs: doc
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "internal server error",
+                error: err
+            })
+        });
 }
