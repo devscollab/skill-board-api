bdfb8b25c58252faa416126d9c7bc657faf3cc3f
bhavansh <bhavanshofficial@gmail.com>
2020-08-30 12:45:14 +0530

    Email template added - need ui for template view

--- app/controllers/email.js
+++ app/controllers/email.js
@@ -1,48 +1,71 @@
 "use strict";
+const fs = require("fs");
 const nodemailer = require("nodemailer");
+const ejs = require("ejs");
+const path = require("path");
 
 const successful_registration = (mailto) => {
-    let msg = "Congratulations, Your Registration is Successful, You can proceed to login now"
-    main(mailto, msg).catch(console.error);
-}
+  let msg =
+    "Congratulations, Your Registration is Successful, You can proceed to login now";
+  main(mailto, msg).catch(console.error);
+};
 
 const application_approved = (mailto) => {
-    let msg = "Congratulations, Your Application is Approved "
-    main(mailto, msg).catch(console.error);
-}
+  let msg = "Congratulations, Your Application is Approved ";
+  main(mailto, msg).catch(console.error);
+};
 
 const application_rejected = (mailto) => {
-    let msg = "Sorry Your Application is Rejected, Please Try to apply again or contact our support "
-    main(mailto, msg).catch(console.error);
-}
+  let msg =
+    "Sorry Your Application is Rejected, Please Try to apply again or contact our support ";
+  main(mailto, msg).catch(console.error);
+};
 
 const forgot_password = (mailto) => {
-    let msg = "Your Request for password has been recieved,Here is Your Secret Login Code"
-    main(mailto, msg).catch(console.error);
-}
+  let msg =
+    "Your Request for password has been recieved,Here is Your Secret Login Code";
+  main(mailto, msg).catch(console.error);
+};
 
 async function main(mailto, messageto) {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL, // generated ethereal user
+      pass: process.env.PASS, // generated ethereal password
+    },
+  });
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL, // generated ethereal user
-            pass: process.env.PASS, // generated ethereal password
-        },
-    });
+  console.log(path.join(__dirname, "../views/test.ejs"));
 
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
+  ejs.renderFile(path.join(__dirname, "../views/test.ejs"), function ( err,data) {
+    if (err) {
+      console.log(err);
+    } else {
+      let mailOptions = {
         from: '"SkillBoard devs team, PCCOE" <skillboard.opensource@gmail.com>', // sender address
         to: mailto, // list of receivers
         subject: "Registration Email", // Subject line
         text: messageto, // plain text body
-        html: messageto, // html body
-    });
-
-    console.log("Message sent: %s", info.messageId);
+        html: data,
+      };
+      console.log("Sending ,mail ======================>");
+      transporter.sendMail(mailOptions, function (err, info) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Message sent: " + info.response);
+        }
+      });
+    }
+  });
 }
 
-
-module.exports = { successful_registration, application_approved, application_rejected, forgot_password, main }
+module.exports = {
+  successful_registration,
+  application_approved,
+  application_rejected,
+  forgot_password,
+  main,
+};
