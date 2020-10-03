a667d52f0cff7aa20dcb52345ffd67c17c81f111
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2020-10-03 09:27:57 +0530

    feat(sort): add student sort route

--- app/routes/student.js
+++ app/routes/student.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
-const studentController = require('../controllers/student');
-const auth = require('../controllers/auth');
+const studentController = require("../controllers/student");
+const auth = require("../controllers/auth");
 
-router.get("/all", auth.studentAuth, studentController.getAllStudentProfiles)
+router.get("/all", auth.studentAuth, studentController.getAllStudentProfiles);
 
-router.get("/:id", auth.studentAuth, studentController.getStudentProfileById)
+router.get("/:id", auth.studentAuth, studentController.getStudentProfileById);
 
-router.patch("/update/:id", auth.studentAuth, studentController.updateStudentProfileById);
+router.patch(
+  "/update/:id",
+  auth.studentAuth,
+  studentController.updateStudentProfileById
+);
 
-router.delete("/delete/:id", auth.studentAuth, studentController.deleteStudentProfileById);
+router.delete(
+  "/delete/:id",
+  auth.studentAuth,
+  studentController.deleteStudentProfileById
+);
 
 router.get("/", auth.studentAuth, studentController.getStudentsByQuery);
+
+router.get(
+  "/sort",
+  auth.studentAuth,
+  studentController.getSortedListOfStudents
+);
 
 module.exports = router;
