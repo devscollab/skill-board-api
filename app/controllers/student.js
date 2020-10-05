6be9f85830e6578b9060e0b358143616c3b98ea7
Jenny Poon <jenny.poon@live.ca>
2020-10-05 12:07:39 -0700

    feat: add function that receives id and rating params and assigns rating to student record

--- app/controllers/student.js
+++ app/controllers/student.js
@@ -126,3 +126,19 @@ exports.updatePassword = async(req, res) => {
         }
     })
 }
+
+exports.setStudentRatingById = (id, rating) =>
+  Student.update({ _id: id }, { $set: { rating: rating } })
+    .exec()
+    .then((doc) => {
+      res.status(200).json({
+        message: 'rating successfully updated',
+        docs: doc,
+      })
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: 'internal server error',
+        error: err,
+      })
+    })
